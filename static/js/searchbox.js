411fb5f4c2ce19cbab6fe1d7ad9a21c0b25da5cd
Shreyas Sood <shreyassood@gmail.com>
2017-04-14 15:59:56 -0500

    Removed generated files

--- static/js/searchbox.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // change input text color appropriately
-    $('#search').focus(function(e) {
-        $(this).removeClass('white-text');
-    }).blur(function(e) {
-        $(this).addClass('white-text');
-    });
-});
